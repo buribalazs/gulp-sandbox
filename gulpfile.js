f61365ad2025018cf83597a7d3e536c536e53bd4
Buri Balázs <buribalazs@gmail.com>
2017-01-18 16:12:43 +0100

    img folder

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,9 @@ var gulp = require('gulp'),
     livereload = require('gulp-livereload');
 
 gulp.task('sass', function () {
-    gulp.src('./src/sass/*.scss')
+    gulp.src('src/sass/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./dist/css'))
+        .pipe(gulp.dest('dist/css'))
         .pipe(livereload());
 });
 
@@ -14,17 +14,26 @@ gulp.task('sass', function () {
 gulp.task('templates', function () {
     var YOUR_LOCALS = {};
 
-    gulp.src('./src/jade/*.jade')
+    gulp.src('src/jade/*.jade')
         .pipe(jade({
             locals: YOUR_LOCALS,
             pretty: true
         }))
-        .pipe(gulp.dest('./dist/'))
+        .pipe(gulp.dest('dist/'))
         .pipe(livereload());
 });
 
-gulp.task('default', ['templates', 'sass'], function () {
+gulp.task('images', function () {
+    var YOUR_LOCALS = {};
+
+    gulp.src('src/img/*.*')
+        .pipe(gulp.dest('dist/img/'))
+        .pipe(livereload());
+});
+
+gulp.task('default', ['templates', 'sass', 'images'], function () {
     livereload.listen();
-    gulp.watch('./src/jade/**/*.jade', ['templates']);
-    gulp.watch('./src/sass/**/*.scss', ['sass']);
+    gulp.watch('jade/**/*.jade', {cwd: './src'}, ['templates']);
+    gulp.watch('sass/**/*.scss', {cwd: './src'}, ['sass']);
+    gulp.watch('img/**/*.*', {cwd: './src'}, ['images']);
 });
