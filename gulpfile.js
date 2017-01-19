2e45028cbb093ebe18dbf26f6ca5109598351bda
Buri Balázs <buribalazs@gmail.com>
2017-01-19 13:53:50 +0100

    improved skeleton

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 var gulp = require('gulp'),
     jade = require('gulp-jade'),
     sass = require('gulp-sass'),
-    livereload = require('gulp-livereload');
+    livereload = require('gulp-livereload'),
+    gutil = require('gulp-util');
+
+    console.log(gutil.env); // --cmd line param
 
 gulp.task('sass', function () {
     gulp.src('src/sass/*.scss')
@@ -24,14 +27,22 @@ gulp.task('templates', function () {
 });
 
 gulp.task('images', function () {
-    var YOUR_LOCALS = {};
-
     gulp.src('src/img/*.*')
         .pipe(gulp.dest('dist/img/'))
         .pipe(livereload());
 });
 
-gulp.task('default', ['templates', 'sass', 'images'], function () {
+gulp.task('jslib', function () {
+    var libs = [
+        'node_modules/jquery/dist/jquery.min.js',
+        'node_modules/itemslide/dist/itemslide.min.js'
+    ];
+    gulp.src(libs).pipe(gulp.dest('dist/js/'));
+});
+
+
+
+gulp.task('default', ['jslib', 'templates', 'sass', 'images'], function () {
     livereload.listen();
     gulp.watch('jade/**/*.jade', {cwd: './src'}, ['templates']);
     gulp.watch('sass/**/*.scss', {cwd: './src'}, ['sass']);
