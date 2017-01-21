3889e8c3dad7b21ad2ee10e1659288aead61e347
buribalazs@gmail.com <buribalazs@gmail.com>
2017-01-21 11:57:56 +0100

    Dóra reviewolta ezt

--- src/js/se-main.js
+++ src/js/se-main.js
@@ -1,3 +1,9 @@
 $(window).on('load', function () {
     $('body').removeClass('preload');
 });
+
+$(function () {
+    $('.stamp-header').html(function () {
+        console.log($(this).text());
+    });
+});
